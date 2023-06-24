e017c72b4b9895b47f2870135b5432e016a6bc25
GeorgeRamos7 <georgepaulozt@gmail.com>
2023-06-24 12:33:21 -0300

    implementa fluxo repository, Teste ok!

--- src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/service/ClienteApplicationService.java
@@ -18,25 +18,21 @@ import lombok.extern.log4j.Log4j2;
 public class ClienteApplicationService implements ClienteService {
 	private final ClienteRepository clienteRepository;
 
-
 	@Override
 	public ClienteResponse criaCliente(ClienteRequest clienteRequest) {
 		log.info("[inicia] ClienteApplicationService - criaCliente");
 		Cliente cliente = clienteRepository.salva(new Cliente(clienteRequest));
 		log.info("[finaliza] ClienteApplicationService - criaCliente");
-		return ClienteResponse
-				.builder()
-				.idCliente(cliente.getIdCliente())
-				.build();
+		return ClienteResponse.builder().idCliente(cliente.getIdCliente()).build();
 	}
-
 
 	@Override
 	public List<ClienteListResponse> buscaTodosClientes() {
 		log.info("[inicia] ClienteApplicationService -buscaTodosClientes");
+		List<Cliente> clientes = clienteRepository.buscaTodosClientes();
 		log.info("[finaliza] ClienteApplicationService - buscaTodosClientes");
-		
-		return null;
+
+		return ClienteListResponse.converte(clientes);
 	}
 
 }
